d1f3c9871d32411cfa92a8231f1c63d96eeb49b8
Nat <40128337+natprogrammer33@users.noreply.github.com>
2018-09-24 20:39:59 -0400

    Added javascript!

---
+++ javascript.js
@@ -0,0 +1,35 @@
+function homescroll(){
+    window.scrollBy({
+        top: 250,
+        left:0,
+        behavior: 'smooth'
+    });
+}
+function researchscroll(){
+    window.scrollBy({
+        top: 675,
+        left:0,
+        behavior: 'smooth'
+    });
+}
+function teachingscroll(){
+    window.scrollBy({
+        top: 900,
+        left:0,
+        behavior: 'smooth'
+    });
+}
+function otherscroll(){
+    window.scrollBy({
+        top: 1800,
+        left:0,
+        behavior: 'smooth'
+    });
+}
+function scrolltotop(){
+    window.scrollBy({
+        top: -10000,
+        left:0,
+        behavior: 'smooth'
+    })
+}
